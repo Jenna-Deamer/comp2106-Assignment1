cacb99ce5b341b6718c2b5a88796a944c0a2dda8
Jenna-Deamer <200529678@student.georgianc.on.ca>
2024-02-03 14:18:05 -0500

    added contact,projects, about views & styles

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,18 @@ router.get("/", (req, res) => {
 });
 
 /* GET about page. */
-router.get("/about", (req, res, next) =>{
+router.get("/about", (req, res) => {
   res.render("about", { title: "About Jenna Deamer" });
+});
+
+/* GET projects page. */
+router.get("/projects", (req, res) => {
+  res.render("projects", { title: "Jenna Deamer Work" });
+});
+
+/* GET contact  page. */
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact Jenna Deamer" });
 });
 
 module.exports = router;
