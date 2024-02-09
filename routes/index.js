f1380201b90384f4de7f748f0bbeaf4e19fd32ee
Jenna Deamer <jennadeamer@gmail.com>
2024-02-09 16:27:40 -0500

    spelling corrections

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("index", { title: "Jenna Deamer Portfolio" });
+  res.render("index", { title: "Jenna Deamer's Portfolio" });
 });
 
 /* GET about page. */
@@ -13,7 +13,7 @@ router.get("/about", (req, res) => {
 
 /* GET projects page. */
 router.get("/projects", (req, res) => {
-  res.render("projects", { title: "Jenna Deamer Work" });
+  res.render("projects", { title: "Jenna Deamer's Work" });
 });
 
 /* GET contact  page. */
